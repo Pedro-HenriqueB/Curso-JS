616b687850299a123231861e24babe3d59c3fdf9
Pedro Henrique <pedrophbs8199@gmail.com>
2022-07-22 12:24:00 -0300

    Botao ponto e inputs tipo float

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -29,6 +29,8 @@ class CalcController{
 
     clearAll(){
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
         this.setLastNumberToDisplay();
     }
 
@@ -122,10 +124,7 @@ class CalcController{
             if(this.isOperator(value)){
                 this.setLastOperation(value);
 
-            } else if(isNaN(value)) {
-                console.log('Outra coisa', value);
-
-            } else {
+            }  else {
                 this.pushOperation(value);
                 this.setLastNumberToDisplay();
             }
@@ -136,7 +135,7 @@ class CalcController{
                 this.pushOperation(value);
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(newValue);
                 this.setLastNumberToDisplay();
             }
         }
@@ -144,6 +143,21 @@ class CalcController{
 
     setError(){
         this.displayCalc = "Error";
+    }
+
+    addDot(){
+        let lastOperation = this.getLastOperation();
+
+        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
+
+        if(this.isOperator(lastOperation) || !lastOperation){
+            this.pushOperation('0.');
+
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
     }
 
     execBtn(value){
@@ -181,7 +195,7 @@ class CalcController{
             break;
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot();
             break;
 
             case '0':
